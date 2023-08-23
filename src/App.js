fa5727304889284713c914b5f5a73149001c2c35
Piyush142k <85387529+Piyush142k@users.noreply.github.com>
2023-08-23 19:01:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import TodoList from './components/TodoList';
+const App =() => {
+    return (
+        <div className='todo-app'>
+          
+            <TodoList />
+
+        
+        
+
+        </div>
+    );
+};
+
+export default App;
